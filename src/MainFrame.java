a3d8bbb929e28e11625ca18467593a91cbfaaa1d
LaunDan <launadaniel2@gmail.com>
2020-03-10 20:27:35 +0100

    create field with greeting

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,19 @@
 import javax.swing.*;
 
-public class MainFrame {
+public class MainFrame extends JFrame{
     private JPanel Greeting;
     private JLabel textLabel;
+
+    public MainFrame(String title){
+        super(title);
+        setSize(100, 100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(Greeting);
+        this.pack();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new MainFrame("Greeting");
+        frame.setVisible(true);
+    }
 }
