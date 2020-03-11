ce0d8264300babc1e25461ced0a7d3c8a4d5e0aa
LaunDan <launadaniel2@gmail.com>
2020-03-11 22:20:19 +0100

    ouk

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,9 +4,10 @@ public class MainFrame extends JFrame{
     private JPanel Greeting;
     private JLabel textLabel;
 
+
     public MainFrame(String title){
         super(title);
-        setSize(100, 100);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(Greeting);
         this.pack();
